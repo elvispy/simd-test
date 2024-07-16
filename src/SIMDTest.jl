3c115bee0a8d5a1a97c5420db7010933b14dc1da
Elvis Aguero <elvis.alexander.aguero.vera@cern.ch>
2024-07-16 16:04:10 +0200

    added for loop inside functions and simd

--- src/SIMDTest.jl
+++ src/SIMDTest.jl
@@ -4,11 +4,17 @@ export saxp!, saxpsimd!
 
 
 function saxp!(A::Matrix{Float64}, B::Matrix{Float64}, C::Matrix{Float64})
-    @. A += B*C
+    for _ in 1:size(A, 1)
+        @. A += B*C
+    end
+    return A
 end
 
 function saxpsimd!(A::Matrix{Float64}, B::Matrix{Float64}, C::Matrix{Float64})
-    @. A += B*C
+    @simd for _ in 1:size(A, 1)
+        @. A += B*C
+    end
+    return A
 end
 
 end
